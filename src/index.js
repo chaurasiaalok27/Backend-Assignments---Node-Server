1476729c60d4196832abe1055099dfb13ea80c41
Alok Chaurasia <chaurasiaalok4@gmail.com>
2020-12-03 15:49:25 +0530

    awnser

--- src/index.js
+++ src/index.js
@@ -1,59 +1,33 @@
-//var http = require("http");
+var http = require("http");
 
-//const httpServer = http.createServer(handleServer);
-
-const express = require('express');
-const app = express();
-app.get('/welcome',(req,res)=>
-{
-    res.writeHead(200,{
-        'content-type':'text/plain'
-    });
-    res.write('Welcome to Dominos!');
-    res.end();
-});
-
-app.get('/contact',(req,res)=>
-{
-    res.writeHead(200,{
-        'content-type':'application/json'
-    });
-    res.write(
-        JSON.stringify({
-        phone: "18602100000",
-       email: "guestcaredominos@jublfood.com"
-      })
-    );
-    res.end();
-});
-
-//function handleServer(req, res) {
-  //  if(req.url.startsWith("/welcome") && req.method === 'GET')
-    //{
-      //  res.writeHead(200,{
-        //    'content-type':'text/plain'
-        //});
-        //res.write('Welcome to Dominos!');
-        //res.end();
-    //}
-    //else if(req.url.startsWith("/contact") && req.method === 'GET')
-    //{
-     //   res.writeHead(200,{
-     //       'content-type':'application/jason'
-     //   });
-     //   res.write(
-     //       JSON.stringify({
-     //       phone: '18602100000',
-     //      email: 'guestcaredominos@jublfood.com'
-       //   })
-       // );
-        //res.end();
-    //}
-    //else {
-     //   res.writeHead(404,{});
-    //    res.end();
-    //}
+const httpServer = http.createServer(handleServer);
+function handleServer(req, res) {
+   if(req.url.startsWith("/welcome") && req.method === 'GET')
+    {
+       res.writeHead(200,{
+           'content-type':'text/plain'
+        });
+        res.write('Welcome to Dominos!');
+        res.end();
+    }
+    else if(req.url.startsWith("/contact") && req.method === 'GET')
+    {
+       res.writeHead(200,{
+           'content-type':'application/jason'
+       });
+       res.write(
+           JSON.stringify({
+           phone: '18602100000',
+          email: 'guestcaredominos@jublfood.com'
+         })
+       );
+        res.end();
+    }
+    else {
+       res.writeHead(404,{});
+       res.end();
+    }
   
-//}
-
-app.listen(8081);
+}
+module.exports = httpServer;
+httpServer.listen(8081);
